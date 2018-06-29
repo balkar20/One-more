9b485aa48dc57bdc3f56add6cf46550a861ae13a
vbalkarov <balkar20@mail.ru>
2018-06-29 12:01:08 +0300

    jhjhjh

---
+++ NewOfices/Save/Servicer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+
+namespace SalaryReport.Save
+{
+    public class Servicer
+    {
+        public XmlDocument GetXmlCurencyData(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(response.GetResponseStream());
+                return xmlDocument;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
